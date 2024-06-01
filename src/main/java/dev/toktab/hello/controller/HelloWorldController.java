133ecc70d08850204a804f5bd713bae6ac457acf
gepeivar <niwif21305@crodity.com>
2024-06-01 15:56:31 +0400

    finish

---
+++ src/main/java/dev/toktab/hello/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package dev.toktab.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "zdaiova babu";
+    }
+}
